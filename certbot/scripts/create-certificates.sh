d0b721b67d5753d64ff6dafb4b88a2fb374a46ef
GooeyTuxedo <enderhex@protonmail.com>
2023-03-03 15:12:55 -0800

    fix: refactor certbot to use dns challenge

--- certbot/scripts/create-certificates.sh
+++ certbot/scripts/create-certificates.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Request certificates
-certbot certonly --standalone \
+certbot certonly --dns-digitalocean \
+  --dns-digitalocean-credentials /digitalocean.ini \
   --non-interactive --agree-tos --email $ADMIN_EMAIL --http-01-port=380 \
   --cert-name $DOMAIN_URL \
-  -d $DOMAIN_URL
+  -d $DOMAIN_URL \
+  -d "ipfs.$DOMAIN_URL" \
+  -d "ipns.$DOMAIN_URL" \
+  -d "*.ipfs.$DOMAIN_URL" \
+  -d "*.ipns.$DOMAIN_URL"
 
 # Concatenate certificates
 . /etc/scripts/concatenate-certificates.sh
