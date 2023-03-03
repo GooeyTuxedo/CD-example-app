1c17bf3c764a95f53cd5579bd5e6b9c8b0396674
GooeyTuxedo <enderhex@protonmail.com>
2023-03-02 18:33:18 -0800

    fix: fetch video correctly

--- nextjs-docker/pages/video.js
+++ nextjs-docker/pages/video.js
@@ -1,32 +1,10 @@
 import { useState, useEffect } from 'react'
-import { create } from 'ipfs-http-client'
 
 const url = process.env.NEXT_PUBLIC_DOMAIN_URL
 const videoHash = process.env.NEXT_PUBLIC_VIDEO_HASH
 
-const client = create({ host: `ipfs.${url}`, port: 8080, protocol: 'https' })
-
 export default function Video() {
-  const [videoUrl, setVideoUrl] = useState(null)
-
-  useEffect(() => {
-    async function getVideoFromIPFS() {
-      try {
-        const { cid } = await client.resolve(`/ipfs/${videoHash}`)
-        const videoUrl = `https://ipfs.${url}/ipfs/${cid.toString()}`
-
-        setVideoUrl(videoUrl)
-      } catch (e) {
-        console.error(e)
-      }
-    }
-
-    getVideoFromIPFS()
-  }, [])
-
-  if (!videoUrl) {
-    return <div>Loading...</div>
-  }
+  const videoUrl = `https://ipfs.${url}/ipfs/${videoHash}`
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
