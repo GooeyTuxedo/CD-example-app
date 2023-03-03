cb2785194f46ea7f958aa053518b4d26f4ca0bdd
GooeyTuxedo <enderhex@protonmail.com>
2023-03-02 19:31:53 -0800

    chore: add configuration for subdomain gateway

--- nextjs-docker/pages/video.js
+++ nextjs-docker/pages/video.js
@@ -1,13 +1,10 @@
-import { useState, useEffect } from 'react'
-
-const url = process.env.NEXT_PUBLIC_DOMAIN_URL
-const videoHash = process.env.NEXT_PUBLIC_VIDEO_HASH
-
 export default function Video() {
-  const videoUrl = `https://ipfs.${url}/ipfs/${videoHash}`
+  const url = process.env.NEXT_PUBLIC_DOMAIN_URL
+  const videoHash = process.env.NEXT_PUBLIC_VIDEO_HASH
+  const videoUrl = `https://${videoHash}.ipfs.${url}`
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
       <video controls>
         <source src={videoUrl} type="video/mp4" />
       </video>
