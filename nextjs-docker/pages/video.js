34b628ff6f94be5e504c4d749e502ddf395081a7
GooeyTuxedo <enderhex@protonmail.com>
2023-03-03 04:03:43 -0800

    fix: match wildcard subdomains on substring

--- nextjs-docker/pages/video.js
+++ nextjs-docker/pages/video.js
@@ -1,10 +1,10 @@
 export default function Video() {
   const url = process.env.NEXT_PUBLIC_DOMAIN_URL
   const videoHash = process.env.NEXT_PUBLIC_VIDEO_HASH
-  const videoUrl = `https://ipfs.${url}/ipfs/${videoHash}`
+  const videoUrl = `https://${videoHash}.ipfs.${url}`
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
       <video controls>
         <source src={videoUrl} type="video/mp4" />
       </video>
