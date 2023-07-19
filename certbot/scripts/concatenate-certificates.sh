ea4689477d1b5acf544295321f0cee83c66d86b5
MrTuxedo <dev@based.consulting>
2023-07-18 17:45:43 -0700

    fix(build): stop copying .env into container

--- certbot/scripts/concatenate-certificates.sh
+++ certbot/scripts/concatenate-certificates.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 if [ -f /etc/letsencrypt/live/$DOMAIN_URL/fullchain.pem -a -f /etc/letsencrypt/live/$DOMAIN_URL/privkey.pem ]; then
   cat /etc/letsencrypt/live/$DOMAIN_URL/fullchain.pem /etc/letsencrypt/live/$DOMAIN_URL/privkey.pem > /etc/certificates/$DOMAIN_URL.pem
-  cp /etc/letsencrypt/live/$DOMAIN_URL/fullchain.pem /etc/letsencrypt/live/$DOMAIN_URL/privkey.pem /etc/certificates/$DOMAIN_URL/
 fi
